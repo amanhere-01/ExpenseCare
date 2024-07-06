d7b79e7846dd601090ef028a585d82dd2fcf5700
amanhere-01 <amanhere0101@gmail.com>
2024-07-06 18:04:01 +0530

    add expense page

---
+++ lib/pages/new_expense.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../color_extension.dart';
+import '../widgets/expense_add.dart';
+
+class NewExpense extends StatelessWidget {
+  const NewExpense({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // backgroundColor: Colors.red,
+        backgroundColor: TColor.peach2,
+      ),
+      backgroundColor: TColor.peach2,
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Add Expense',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+            ),
+          ),
+          const SizedBox(height: 50,),
+          const Expanded(
+            child: AddExpense()
+          ),
+        ],
+      ),
+    );
+  }
+}
