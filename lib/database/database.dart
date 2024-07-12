cd530d4c643b7a8746ca120c704ac8e3414bee48
amanhere-01 <amanhere0101@gmail.com>
2024-07-12 22:41:05 +0530

    new query

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,10 +1,12 @@
 
 import 'dart:io';
+import 'dart:math';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path ;
+import 'package:provider/provider.dart';
 import 'package:sqlite3/sqlite3.dart';
 import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
@@ -27,13 +29,76 @@ class AppDatabase extends _$AppDatabase {
   @override
   int get schemaVersion => 1;
 
-  Future<List<Expense>> getAllExpenses() async => await select(expenses).get();
-  Future insertExpense(ExpensesCompanion entity) async => await into(expenses).insert(entity);
-  Future updateExpense(ExpensesCompanion entity) async => await update(expenses).replace(entity);
-  Future deleteExpense(Expense entity) async => await delete(expenses).delete(entity);
+  Future<List<Expense>> getAllExpenses() async {
+    return await select(expenses).get();
+  }
 
+  Future insertExpense(ExpensesCompanion entity) async {
+    return await into(expenses).insert(entity);
+  }
 
+  Future updateExpense(ExpensesCompanion entity) async {
+    return await update(expenses).replace(entity);
+  }
+
+  Future deleteExpense(Expense entity) async {
+    return await delete(expenses).delete(entity);
+  }
+
+  Future<double> totalExpenses() async {
+    final total = expenses.amount.sum();
+    final query= selectOnly(expenses)..addColumns([total]);
+    return await query.map((row) => row.read(total) ?? 0.0).getSingle();
+  }
+
+  Future<double> todayExpenses() async {
+    final now = DateTime.now();
+    final startOfDay= DateTime(now.year,now.month,now.day);
+    final endOfDay= DateTime(now.year,now.month,now.day, 23, 59, 59);
+
+    final total = expenses.amount.sum();
+    final query= selectOnly(expenses)..addColumns([total])..where(expenses.date.isBetweenValues(startOfDay, endOfDay));
+    return query.map((r) => r.read(total) ?? 0.0).getSingle();
+  }
+
+  Future<double> totalMonthExpenses() async {
+    final now = DateTime.now();
+    final firstDayMonth = DateTime(now.year, now.month, 1);
+    final lastDayMonth = DateTime(now.year, now.month + 1, 0, 23, 59, 59);
+
+    final total = expenses.amount.sum();
+    final query = selectOnly(expenses)
+      ..addColumns([total])
+      ..where(expenses.date.isBetweenValues(firstDayMonth, lastDayMonth));
+
+    return await query.map((row) => row.read(total) ?? 0.0).getSingle();
+  }
+
+  Future<double> monthExpenses(int monthNumber) async{
+    final now = DateTime.now();
+
+    final total = expenses.amount.sum();
+    final query = selectOnly(expenses)
+      ..addColumns([total])
+      ..where(expenses.date.month.equals(monthNumber) & expenses.date.year.equals(now.year));
+
+    return await query.map((row) => row.read(total) ?? 0.0).getSingle();
+  }
+
+  Future<double> monthlyCategoryExpenses(int monthNumber, String category) async{
+    final now = DateTime.now();
+
+    final total = expenses.amount.sum();
+    final query = selectOnly(expenses)
+      ..addColumns([total])
+      ..where(expenses.date.month.equals(monthNumber) & expenses.date.year.equals(now.year) & expenses.category.equals(category));
+
+    return await query.map((row) => row.read(total) ?? 0.0).getSingle();
+  }
 }
+
+
+
 
 LazyDatabase _openConnection(){
   return LazyDatabase(() async {
