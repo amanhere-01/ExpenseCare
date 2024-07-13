b4913ff0338b51c65c1466e8d01c425fb8ddbe3f
amanhere-01 <amanhere0101@gmail.com>
2024-07-13 23:37:50 +0530

    bug fixes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,106 +1,106 @@
 import 'package:expense_care/color_extension.dart';
-import 'package:expense_care/pages/all_transaction.dart';
-import 'package:expense_care/pages/new_expense.dart';
-import 'package:expense_care/widgets/recent_transactions.dart';
-import 'package:fl_chart/fl_chart.dart';
+import 'package:expense_care/providers/expense_provider.dart';
+import 'package:expense_care/widgets/expense_pie_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-          body: SafeArea(
-            child: Column(
-              children: [
-                const SizedBox(height: 20,),
-                Center(
-                  child: Text(
-                    "Total Expense",
-                    style: Theme.of(context).textTheme.bodySmall,
-                  )
-                ),
-                Text(
-                  '₹ 2000',
-                  style: Theme.of(context).textTheme.displayLarge ,
-                ),
-                // Flexible(
-                //   flex: 2,
-                //   child: BarChart(
-                //     swapAnimationDuration: const Duration(seconds:1),
-                //       BarChartData(
-                //         // titlesData: [
-                //         //
-                //         // ]
-                //       )
-                //   ),
-                // ),
-                Flexible(
-                  flex: 2,
-                  child: Container(
-                    padding:  const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                    decoration:   BoxDecoration(
-                      color: Colors.white60,
-                      borderRadius: BorderRadius.vertical(top: Radius.circular(30))
-                    ) ,
-                    child:  Column(
-                      children: [
-                        Row(
-                          children: [
-                            Text('Recent Transactions',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                                color: TColor.text
-                              ),
-                            ),
-                            const Spacer(),
-                            TextButton(
-                                onPressed: (){
-                                  Navigator.push(context, MaterialPageRoute(builder: (context) => AllTransaction()));
-                                },
-                                style: TextButton.styleFrom(
-                                  backgroundColor: TColor.secondaryText,
-                                  foregroundColor: Colors.black
-                                ),
-                                child: const Text('See All',)
-                            )
-                          ],
-                        ),
-                        const RecentTransactions()
-                        // Expanded(
-                        //   child: RecentTransactions()
-                        // )
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            )
-          ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: const [
-      //     BottomNavigationBarItem(
-      //         icon: Icon(Icons.home_filled),
-      //       label: 'Home'
-      //     ),
-      //     BottomNavigationBarItem(
-      //         icon: Icon(Icons.credit_card),
-      //         label: 'Transactions'
-      //     ),
-      //   ],
-      // ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: TColor.secondaryText,
-        onPressed: (){
-          Navigator.push(context,MaterialPageRoute(builder:(context) => const NewExpense()));
-        },
-        child: const Icon(Icons.add),
+  State<HomePage> createState() => _HomePageState();
+}
 
-      )
-    );
+class _HomePageState extends State<HomePage> {
+  final List<Map<String, dynamic>> monthList = [
+    {'name': 'January', 'number': 1},
+    {'name': 'February', 'number': 2},
+    {'name': 'March', 'number': 3},
+    {'name': 'April', 'number': 4},
+    {'name': 'May', 'number': 5},
+    {'name': 'June', 'number': 6},
+    {'name': 'July', 'number': 7},
+    {'name': 'August', 'number': 8},
+    {'name': 'September', 'number': 9},
+    {'name': 'October', 'number': 10},
+    {'name': 'November', 'number': 11},
+    {'name': 'December', 'number': 12},
+  ];
+
+  late String selectedMonth;
+  late int selectedMonthNumber;
+
+  @override
+  void initState(){
+    super.initState();
+    selectedMonth= DateFormat.MMMM().format(DateTime.now());
+    selectedMonthNumber= DateTime.now().month;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 40,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      width: 1,
+                      color: TColor.white2
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                  color: TColor.black4
+              ),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton(
+                  value: selectedMonth,
+                  borderRadius: BorderRadius.circular(20),
+                  dropdownColor: TColor.black6,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: TColor.white4,
+                      fontWeight: FontWeight.w500
+                  ),
+                  icon: Icon(Icons.keyboard_arrow_down_rounded,color: TColor.white4,),
+                  iconSize: 20,
+                  onChanged: (String? value) {
+                    setState(() {
+                      selectedMonth=value!;
+                      selectedMonthNumber= monthList.firstWhere((month) => month['name']==selectedMonth)['number'];
+                    });
+                  },
+                  items: monthList.map<DropdownMenuItem<String>>((Map<String,dynamic> month){
+                    return DropdownMenuItem(
+                        value: month['name'],
+                        child: Text(month['name'])
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+            FutureBuilder(
+              future: Provider.of<ExpenseProvider>(context).monthExpenses(selectedMonthNumber),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator()); // Loading state
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}')); // Error state
+                } else if (snapshot.hasData) {
+                  return Expanded(
+                    child: ExpensePieChart(monthNumber: selectedMonthNumber, totalMonthExpense: snapshot.data!,),
+                  );
+                } else {
+                  return const Center(child: Text('No data available')); // Handle cases with no data
+                }
+              },
+            ),
+          ],
+        )
+      );
   }
 }
 
