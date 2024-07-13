d3b99c6bf84aa6d352aa73f71d04b0cfc4f0c78d
amanhere-01 <amanhere0101@gmail.com>
2024-07-13 23:40:58 +0530

    query added

--- lib/providers/expense_provider.dart
+++ lib/providers/expense_provider.dart
@@ -1,15 +1,54 @@
+import 'package:expense_care/database/database.dart';
 import 'package:flutter/material.dart';
 
-class ExpenseList extends ChangeNotifier{
-  final List<Map<String,dynamic>> expenses =[];
+class ExpenseProvider extends ChangeNotifier{
+  final AppDatabase _database = AppDatabase();
+  List<Expense> _expenses = [];
 
-  void addExpense(Map<String,dynamic> expense){
-    expenses.add(expense);
+  List<Expense> get expenses => _expenses;
+
+  ExpenseProvider(){
+    _loadExpenses();
+  }
+
+  Future<void>  _loadExpenses()  async{
+    _expenses = await _database.getAllExpenses();
     notifyListeners();
   }
-  void deleteExpense(Map<String,dynamic> expense){
-    expenses.remove(expense);
-    notifyListeners();
+
+  Future<void> addExpense(ExpensesCompanion expense) async{
+    await _database.insertExpense(expense);
+    await _loadExpenses();
   }
+
+  Future<void> deleteExpense(Expense expense) async{
+    await _database.deleteExpense(expense);
+    await _loadExpenses();
+  }
+
+  Future<void> updateExpense(ExpensesCompanion expense) async{
+    await _database.updateExpense(expense);
+    await _loadExpenses();
+  }
+
+  Future<double> totalExpenses() async{
+    return await _database.totalExpenses();
+  }
+
+  Future<double> todayExpenses() async{
+    return await _database.todayExpenses();
+  }
+
+  Future<double> totalMonthExpenses() async{
+    return await _database.totalMonthExpenses();
+  }
+
+  Future<double> monthExpenses(int monthNumber) async{
+    return await _database.monthExpenses(monthNumber);
+  }
+  Future<double> monthlyCategoryExpenses(int monthNumber, String category) async{
+    return await _database.monthlyCategoryExpenses(monthNumber,category);
+  }
+
 
 }
