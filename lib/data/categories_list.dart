f37b46bb3bc22356196878a73b647e687c5d2aa7
amanhere-01 <amanhere0101@gmail.com>
2024-07-15 16:32:58 +0530

    data

---
+++ lib/data/categories_list.dart
@@ -0,0 +1,53 @@
+final categoriesType = [
+  {
+    'title' : 'Food',
+    'imageUrl' : 'assets/images/food.png',
+    'color' : 0xffFF0000
+  },
+  {
+    'title' : 'Grocery',
+    'imageUrl' : 'assets/images/grocery.png',
+    'color' : 0xff00FF00
+  },
+  {
+    'title' : 'Shopping',
+    'imageUrl' : 'assets/images/shopping.png',
+    'color' : 0xff9370DB
+  },
+  {
+    'title' : 'Travel',
+    'imageUrl' : 'assets/images/travel.png',
+    'color' : 0xffFFFF00
+  },
+  {
+    'title' : 'Entertainment',
+    'imageUrl' : 'assets/images/entertainment.png',
+    'color' : 0xff00FFFF
+  },
+  {
+    'title' : 'Bills',
+    'imageUrl' : 'assets/images/bills.png',
+    'color' : 0xffFF00FF
+  },
+  {
+    'title' : 'Beauty',
+    'imageUrl' : 'assets/images/beauty.png',
+    'color' : 0xffFF1493
+  },
+  {
+    'title' : 'Health',
+    'imageUrl' : 'assets/images/health.png',
+    'color' : 0xffFFA500
+  },
+  {
+    'title' : 'Repair',
+    'imageUrl' : 'assets/images/repair.png',
+    'color' : 0xffFFD700
+  },
+  {
+    'title' : 'Others',
+    'imageUrl' : 'assets/images/others.png',
+    'color' : 0xff228B22
+  },
+
+];
---
+++ lib/data/color_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class TColor {
+  static Color get white1 => const Color.fromRGBO(252, 252, 252, 1);
+  static Color get white2 => const Color.fromRGBO(242, 242, 242, 1.0);
+  static Color get white3=> const Color.fromRGBO(232, 232, 232, 1);
+  static Color get white4 => const Color.fromRGBO(220, 220, 220, 1);
+  static Color get white5 => const Color(0xFFBDBDBD);
+  static Color get white6 => const Color.fromRGBO(148, 148, 156, 1);
+  static Color get white7 => const Color(0xFF757575);
+  static Color get white8 => const Color.fromRGBO(45 , 44, 49, 1);
+
+  static Color get black1 => const Color(0xFF000000);
+  static Color get black2 => const Color.fromRGBO(16, 12, 18, 1);
+  static Color get black3 => const Color(0xFF100E0E);
+  static Color get black4 => const Color(0xFF151414);
+  static Color get black5 => const Color.fromRGBO(37, 36, 36, 1.0);
+  static Color get black6 => const Color(0xFF2C2C2C);
+  static Color get black7 => const Color(0xFF3C3C3C);
+  static Color get black8 => const Color(0xFF555555);
+  static Color get black9 => const Color(0xFF888888);
+  static Color get black10 => const Color(0xFFAAAAAA);
+
+  static Color get text => const Color.fromRGBO(45 , 44, 49, 1);
+  static Color get secondaryText => const Color.fromRGBO(148, 148, 156, 1);
+
+
+  static Color get blue1 => const Color(0xffa7d5d5);
+  static Color get blue2 => const Color.fromRGBO(162, 208, 226, 1);
+  static Color get blue3 => const Color(0xffAFEEEE);
+  static Color get blue4 => const Color(0xff98F5FF);
+
+  static Color get cyan => const Color.fromRGBO(239, 255, 255, 1);
+  static Color get cyan1 => const Color.fromRGBO(229, 255, 255, 1);
+  static Color get cyan2 => const Color.fromRGBO(204, 255, 255, 1);
+  static Color get cyan3 => const Color(0xff00FAD9);
+
+
+  static Color get peach => const Color(0xffFFD2CC);
+  static Color get peach1 => const Color(0xffFFA699);
+  static Color get peach2 => const Color(0xffbe6b63);
+
+  static Color get pink => const Color.fromRGBO(246, 229, 237, 1.0);
+  static Color get pink1 => const Color.fromRGBO(236, 216, 227, 1.0);
+
+  static Color get lightGreen => const Color.fromRGBO(224, 245, 231, 1.0);
+  static Color get green => const Color.fromRGBO(208, 247, 226, 1);
+  static Color get green1 => const Color.fromRGBO(173, 247, 207, 1);
+  static Color get green2 => const Color.fromRGBO(41, 135, 85, 1);
+
+  static Color get red => const  Color.fromRGBO(245, 198, 196, 1);
+  static Color get red1 => const Color.fromRGBO(235, 61, 49, 1);
+
+}
